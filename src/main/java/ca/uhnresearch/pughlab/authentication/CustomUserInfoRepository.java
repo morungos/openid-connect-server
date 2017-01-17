1136d23464cbdb265d26f07804e618faadeceb9c
Stuart Watt <stuart@morungos.com>
2017-01-17 09:44:08 -0500

    Add the email verified value

--- src/main/java/ca/uhnresearch/pughlab/authentication/CustomUserInfoRepository.java
+++ src/main/java/ca/uhnresearch/pughlab/authentication/CustomUserInfoRepository.java
@@ -17,13 +17,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * A modified JpaUserInfoRepository which behaves very much as if it is a 
+ * A modified JpaUserInfoRepository which behaves very much as if it is a
  * persisted cache of user info data. When we look for an item and fail to
  * find it, rather than just failing, we look to see if we are currently
  * authenticated, and if so, we map the LDAP data into a UserInfo object
  * and persist that. This means that on first access we get a new saved
- * user record to back up our tokens. 
- * 
+ * user record to back up our tokens.
+ *
  * @author Stuart Watt
  *
  */
@@ -37,33 +37,33 @@ public class CustomUserInfoRepository implements UserInfoRepository {
 	private String adminDisplayName = null;
 	private String adminGivenName = null;
 	private String adminFamilyName = null;
-	
+
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	private UserInfo getUserInfoFromPrincipal(String username) throws IllegalArgumentException {
-		
+
 		log.info("Loading user info for {}", username);
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		
+
 		if (principal instanceof LdapAccountDetails) {
 			LdapAccountDetails person = (LdapAccountDetails) principal;
 			UserInfo ui = new DefaultUserInfo();
 			if (person.getUsername().equals(username)) {
 				ui.setPreferredUsername(person.getUsername());
 				ui.setSub(person.getUsername());
-				
+
 				final String mail = person.getAttributeAsString("mail");
 				if (mail != null) {
 					ui.setEmail(mail);
 					ui.setEmailVerified(false);
 				}
-				
+
 				final String displayName = person.getAttributeAsString("displayName");
 				if (displayName != null) {
 					ui.setName(displayName);
 				}
-				
+
 				final String givenName = person.getAttributeAsString("givenName");
 				if (givenName != null) {
 					ui.setGivenName(givenName);
@@ -86,17 +86,18 @@ public class CustomUserInfoRepository implements UserInfoRepository {
 			ui.setPreferredUsername(adminPrincipal);
 			ui.setSub(adminPrincipal);
 			ui.setEmail(adminMail);
+			ui.setEmailVerified(false);
 			ui.setName(adminDisplayName);
 			ui.setGivenName(adminGivenName);
 			ui.setFamilyName(adminFamilyName);
 			return ui;
 		}
-		throw new IllegalArgumentException("User not found: " + username);			
+		throw new IllegalArgumentException("User not found: " + username);
 	}
-	
+
 	/**
-	 * Looks for a UserInfo record, and if found returns it, otherwise a new record is generated and 
-	 * saved if the currently authenticated user matches the key and value. 
+	 * Looks for a UserInfo record, and if found returns it, otherwise a new record is generated and
+	 * saved if the currently authenticated user matches the key and value.
 	 * @param query
 	 * @param key
 	 * @param value
@@ -120,22 +121,22 @@ public class CustomUserInfoRepository implements UserInfoRepository {
 	@Transactional
 	public UserInfo getByUsername(String username) {
 		log.info("getByUsername for {}", username);
-		
+
 		// Logic here is a wee bit harder. If we can find the object, we should return it.
-		// If not, we should use getUserInfoFromPrincipal() and persist it. 
-		
+		// If not, we should use getUserInfoFromPrincipal() and persist it.
+
 		UserInfo found = null;
 		try {
 			TypedQuery<DefaultUserInfo> query = manager.createNamedQuery("DefaultUserInfo.getByUsername", DefaultUserInfo.class);
 			query.setParameter("username", username);
 			found = JpaUtil.getSingleResult(query.getResultList());
-			
+
 			if (found == null) {
 				found = getUserInfoFromPrincipal(username);
 				DefaultUserInfo dui = (DefaultUserInfo)found;
 				JpaUtil.saveOrUpdate(dui.getId(), manager, dui);
 			}
-			
+
 		} catch (IllegalArgumentException e) {
 			// Nothing needed here
 		}
